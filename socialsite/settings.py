ed80acafa364f72ccb21bcd5a922acdb887afbee
Ratnesh Kushwaha <ratnesh.kush@yahoo.in>
2018-08-04 11:59:55 +0530

    Credentials removed

--- socialsite/settings.py
+++ socialsite/settings.py
@@ -130,6 +130,6 @@ LOGOUT_URL = reverse_lazy('accounts:logout')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'rat.kush@gmail.com'
-EMAIL_HOST_PASSWORD = 'aquarius17'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT= 587
 EMAIL_USE_TLS = True
